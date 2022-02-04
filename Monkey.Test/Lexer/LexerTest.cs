4ff633f48d751a00e993b9021483fc6533b5afaf
stephen <sstucky3@gmail.com>
2022-02-03 22:07:51 -0600

    small program test

--- Monkey.Test/Lexer/LexerTest.cs
+++ Monkey.Test/Lexer/LexerTest.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -6,19 +8,21 @@ namespace Monkey.Test.Lexer;
 
 public class LexerTest
 {
+    private const string SmallProgramFilePath = "small_program.txt";
+    
     [Test]
-    public void Should_Lex_Next_Token()
+    public void Should_Lex_Simple_Tokens()
     {
         var input = "=+(){}";
 
         var expected = new List<Token>
         {
-            new (TokenTypes.Operators.Assign, "="),
-            new (TokenTypes.Operators.Plus, "+"),
-            new (TokenTypes.Delimiters.LParen, "("),
-            new (TokenTypes.Delimiters.RParen, ")"),
-            new (TokenTypes.Delimiters.LBrace, "{"),
-            new (TokenTypes.Delimiters.RBrace, "}")
+            new(TokenTypes.Operators.Assign, "="),
+            new(TokenTypes.Operators.Plus, "+"),
+            new(TokenTypes.Delimiters.LParen, "("),
+            new(TokenTypes.Delimiters.RParen, ")"),
+            new(TokenTypes.Delimiters.LBrace, "{"),
+            new(TokenTypes.Delimiters.RBrace, "}")
         };
 
         //for some reason this has to be fully qualified...
@@ -29,5 +33,69 @@ public class LexerTest
             token.Type.Should().Be(expectedToken.Type);
             token.Literal.Should().Be(expectedToken.Literal);
         }
+    }
+
+    [Test]
+    public void Should_Lex_Small_Program()
+    {
+        //TODO: just read this from a file.
+        var input = @"let five = 5;
+        let ten = 10;
+
+        let add = fn(x,y) {
+            x + y;
+        }
+
+        let result = add(five, ten);";
+
+        var expected = new List<Token>
+        {
+            new(TokenTypes.Keywords.Let, "let"),
+            new(TokenTypes.Literals.Ident, "five"),
+            new(TokenTypes.Operators.Assign, "="),
+            new(TokenTypes.Literals.Int, "5"),
+            new(TokenTypes.Delimiters.Semicolon, ";"),
+            new(TokenTypes.Keywords.Let, "let"),
+            new(TokenTypes.Literals.Ident, "ten"),
+            new(TokenTypes.Operators.Assign, "="),
+            new(TokenTypes.Literals.Int, "10"),
+            new(TokenTypes.Delimiters.Semicolon, ";"),
+            new(TokenTypes.Keywords.Let, "let"),
+            new(TokenTypes.Literals.Ident, "add"),
+            new(TokenTypes.Operators.Assign, "="),
+            new(TokenTypes.Keywords.Function, "fn"),
+            new(TokenTypes.Delimiters.LParen, "("),
+            new(TokenTypes.Literals.Ident, "x"),
+            new(TokenTypes.Delimiters.Comma, ","),
+            new(TokenTypes.Literals.Ident, "y"),
+            new(TokenTypes.Delimiters.RParen, ")"),
+            new(TokenTypes.Delimiters.LParen, "("),
+            new(TokenTypes.Literals.Ident, "x"),
+            new(TokenTypes.Operators.Plus, "+"),
+            new(TokenTypes.Literals.Ident, "y"),
+            new(TokenTypes.Delimiters.Semicolon, ";"),
+            new(TokenTypes.Delimiters.RBrace, "}"),
+            new(TokenTypes.Delimiters.Semicolon, ";"),
+            new(TokenTypes.Keywords.Let, "let"),
+            new(TokenTypes.Literals.Ident, "result"),
+            new(TokenTypes.Operators.Assign, "="),
+            new(TokenTypes.Literals.Ident, "add"),
+            new(TokenTypes.Delimiters.LParen, "("),
+            new(TokenTypes.Literals.Ident, "five"),
+            new(TokenTypes.Delimiters.Comma, ","),
+            new(TokenTypes.Literals.Ident, "ten"),
+            new(TokenTypes.Delimiters.RParen, ")"),
+            new(TokenTypes.Delimiters.Semicolon, ";"),
+            new(TokenTypes.Eof, ""),
+        };
+        
+        var lexer = new Monkey.Lexer(input);
+        foreach (var expectedToken in expected)
+        {
+            var token = lexer.NextToken();
+            token.Type.Should().Be(expectedToken.Type);
+            token.Literal.Should().Be(expectedToken.Literal);
+        }
+
     }
 }
