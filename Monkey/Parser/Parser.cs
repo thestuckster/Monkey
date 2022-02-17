6c36cd62158c6f14eb3684ff5ff0926789c07f84
stephen <sstucky3@gmail.com>
2022-02-16 23:53:46 -0600

    time to p-p-p-parse

--- Monkey/Parser/Parser.cs
+++ Monkey/Parser/Parser.cs
@@ -14,12 +14,19 @@ public class Parser
         //read in two tokens so currentToken and peekToken are both set.
         NextToken();
         NextToken();
-        
     }
 
     public AProgram ParseAProgram()
     {
-        return null;
+        var program = new AProgram();
+
+        
+        while (_lexer.HasNextToken())
+        {
+            Console.WriteLine("blahblahblahblah");
+        }
+        
+        return program;
     }
     
     private void NextToken()
