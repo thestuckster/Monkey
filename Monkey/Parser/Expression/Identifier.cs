0ac58bcab89d2820f43b1b204449cc9dec8697ab
stephen <sstucky3@gmail.com>
2022-02-16 23:31:40 -0600

    Comments to help explain the madness

--- Monkey/Parser/Expression/Identifier.cs
+++ Monkey/Parser/Expression/Identifier.cs
@@ -1,11 +1,17 @@
-using System.Reflection.Metadata.Ecma335;
-
 namespace Monkey.Parser;
 
+/// <summary>
+/// Identifier is an expression because in monkey some parts of an identifier DO produce values.
+/// Ex: let x = ValueProducingIdentifier;
+///
+/// To keep the number of different nodes to a minimum we will use Identifier to represent the name in a variable binding
+/// and later reuse it, to represent an identifier as part of, or, as a complete expression.
+/// </summary>
 public class Identifier : Expression
 {
-    public Token Token { get; set; }
+    public Token Token { get; set; } //the IDENT token
     public string Value { get; set; }
+    
     public string TokenLiteral() => Token.Literal;
     
 }
--- Monkey/Parser/Statements/LetStatement.cs
+++ Monkey/Parser/Statements/LetStatement.cs
@@ -2,9 +2,9 @@ namespace Monkey.Parser;
 
 public class LetStatement : Statement
 {
-    public Token Token { get; set; }
-    public Identifier Name { get; set; }
-    public Expression Value { get; set; }
+    public Token Token { get; set; } //the LET token
+    public Identifier Name { get; set; } //the x in let x = 5;
+    public Expression Value { get; set; } // the 
 
     public string TokenLiteral() => Token.Literal;
 }
