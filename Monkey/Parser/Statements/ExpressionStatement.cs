31e3355772e911da9093d97cdbae0ff150149cc6
stephen <sstucky3@gmail.com>
2022-03-08 22:29:39 -0600

    doc comment for clarification

--- Monkey/Parser/Statements/ExpressionStatement.cs
+++ Monkey/Parser/Statements/ExpressionStatement.cs
@@ -1,5 +1,9 @@
 namespace Monkey.Parser;
 
+/// <summary>
+/// Represents statements that contains expressions.
+/// ie: let myVar = anotherVar; let x = 5 * 5; etc.
+/// </summary>
 public class ExpressionStatement : IStatement
 {
     public Token? Token { get; set; }
