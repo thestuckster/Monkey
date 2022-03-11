1c57c2de71045918266292441d377488e8499f57
stephen <sstucky3@gmail.com>
2022-03-11 17:37:18 -0600

    failing int literal test

--- Monkey.Test/Parser/Expression/ExpressionTest.cs
+++ Monkey.Test/Parser/Expression/ExpressionTest.cs
@@ -33,4 +33,31 @@ public class ExpressionTest
         identifierStatement.Value.Should().Be("foobar");
         identifierStatement.TokenLiteral().Should().Be("foobar");
     }
+
+    [Test]
+    public void ShouldParseIntegerLiteral()
+    {
+        var input = "5;";
+
+        var lexer = new Monkey.Lexer(input);
+        var parser = new Monkey.Parser.Parser(lexer);
+
+        var program = parser.ParseProgram();
+        ParserTestHelper.ParserShouldNotHaveErrors(parser);
+
+        program.Statements.Should().NotBeEmpty();
+        
+        var statement = program.Statements[0];
+        statement.Should().NotBeNull();
+
+        var expressionStatement = statement as ExpressionStatement;
+        expressionStatement.Should().NotBeNull();
+        expressionStatement.Expression.Should().NotBeNull();
+
+        var identifierStatement = expressionStatement.Expression as IntegerLiteral;
+        identifierStatement.Should().NotBeNull();
+
+        identifierStatement.Value.Should().Be(5);
+        identifierStatement.TokenLiteral().Should().Be("5");
+    }
 }
