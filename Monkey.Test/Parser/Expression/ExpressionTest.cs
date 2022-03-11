fe534a6efbf863c2c280a2553f6d22a56c9864d3
stephen <sstucky3@gmail.com>
2022-03-11 16:17:11 -0600

    test parsing identifier statements

---
+++ Monkey.Test/Parser/Expression/ExpressionTest.cs
@@ -0,0 +1,32 @@
+using FluentAssertions;
+using Monkey.Parser;
+using NUnit.Framework;
+
+namespace Monkey.Test.Parser.Expression;
+
+public class ExpressionTest
+{
+    [Test]
+    public void ShouldParseIdentifierExpressions()
+    {
+        var input = "foobar;";
+
+        var lexer = new Monkey.Lexer(input);
+        var parser = new Monkey.Parser.Parser(lexer);
+        
+        var program = parser.ParseProgram();
+        ParserTestHelper.ParserShouldNotHaveErrors(parser);
+
+        var statements = program.Statements;
+        statements.Should().NotBeEmpty();
+
+        var statement = statements[0];
+        statement.Should().NotBeNull();
+
+        var identifierStatement = statement as Identifier;
+        identifierStatement.Should().NotBeNull();
+
+        identifierStatement.Value.Should().Be("foobar");
+        identifierStatement.TokenLiteral().Should().Be("foobar");
+    }
+}
