b6fdf9aa73d8925d99af79e9f1bfa70a31ccc0d9
stephen <sstucky3@gmail.com>
2022-03-11 18:12:45 -0600

    PrefixExpression data type

---
+++ Monkey/Parser/Expression/PrefixExpression.cs
@@ -0,0 +1,24 @@
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Monkey.Parser;
+
+public class PrefixExpression : IExpression
+{
+    public Token Token { get; init; }
+    public string Operator { get; init; }
+
+    public Expression Right { get; set; }
+    
+    public string TokenLiteral() => Token.Literal;
+
+
+    public override string ToString()
+    {
+        var builder = new StringBuilder("(");
+        builder.Append(Operator);
+        builder.Append(Right);
+        builder.Append(")");
+        return builder.ToString();
+    }
+}
