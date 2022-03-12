1f3d242fa802f0a4e08074329239972699f98e0e
stephen <sstucky3@gmail.com>
2022-03-11 18:16:50 -0600

    print our parser errors on test failure

--- Monkey.Test/Parser/ParserTestHelper.cs
+++ Monkey.Test/Parser/ParserTestHelper.cs
@@ -1,11 +1,20 @@
+using System;
+using System.Linq;
 using FluentAssertions;
 
 namespace Monkey.Test.Parser;
 
 public static class ParserTestHelper
 {
-    public static void ParserShouldNotHaveErrors(Monkey.Parser.Parser p) =>
-        p.Errors.Should().HaveCount(0);
+    public static void ParserShouldNotHaveErrors(Monkey.Parser.Parser p)
+    {
+        if (p.Errors.Any())
+        {
+            p.Errors.ForEach(Console.WriteLine);
+        }
+
+        p.Errors.Should().BeEmpty();
+    }
 
     public static void ParserShouldHaveErrors(Monkey.Parser.Parser p, int numberOfErrors) =>
         p.Errors.Should().HaveCount(numberOfErrors);
